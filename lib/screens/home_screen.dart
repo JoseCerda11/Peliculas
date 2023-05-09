4142e63aea61b4c9dee8494116a7d9e5921c4cff
JOSE CERDA PC ESCRITORIO CASA <joseenriquegut11@yahoo.com>
2023-05-08 21:46:37 -0600

    Mi primer commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('Peliculas en cines'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+          ],
+        ),
+        body: Column(
+          children: [
+            //TODO: CardSwiper
+            CardSwiper()
+
+            //TODO: Listado horizontal de peliculas
+          ],
+        ));
+  }
+}
